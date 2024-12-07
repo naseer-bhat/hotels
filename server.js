01e920bc304707a437c8f32b5a61c65230d642bb
naseer-bhat <studymail2k18@gmail.com>
2024-12-07 18:25:48 +0530

    addedport

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 import express from 'express'
+import dotenv from 'dotenv';
 import { connect } from "mongoose";
 import personRouter from './routes/PersonRouter.js';
 import menuRouter from './routes/menuRouter.js';
 // import bodyParser from 'body-parser';
 const app= express()
+dotenv.config()
+const database_URI=process.env.db_url
+const PORT= process.env.PORT ||3000
+
 // app.use(bodyParser.json())
-const database_URI= "mongodb://localhost:27017/hotels"
 connect(database_URI).then(()=>console.log('db connected'))
 app.use(express.json())
 app.use('/person',personRouter)
 app.use('/menu',menuRouter)
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log(`Server is running`)
 })
